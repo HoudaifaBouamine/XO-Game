fe31010dfab02d1f34b0d352656525a7b5b56761
HoudaifaBouamine <houdaifa.bouamine@gmail.com>
2024-06-28 12:06:34 +0100

    create the data model

--- SvelteKitVersion/src/routes/+page.svelte
+++ SvelteKitVersion/src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const joinGame = ()=>
+    {
+
+    }
+
+    // build-in guid
+    
+</script>
+
+<div class="page">
+
+    <h1>XO Game</h1>
+
+    <div class="container">
+        <button id="start" on:click={()=>joinGame()}>
+            Start Playing
+        </button>
+        <button id="exit">
+            Exit Game
+        </button>
+    </div>
+
+</div>
+
+
+<style>
+
+    h1
+    {
+        width: 331px;
+        margin: 0px;
+        padding: 0px;
+        color: #fff;
+        font-size: 76px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+    }
+
+    .page
+    {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap:23px;
+        width: 362px;
+        height: 362px;
+        flex-shrink: 0;
+        border-radius: 40px;
+        background: #aaa;
+        align-items: center;
+        justify-content: center;
+        padding: 0px;
+    }
+
+    .container button {
+        
+        width: 320px;
+        height: 68px;
+        color: #FFF;
+        font-size: 34px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+        
+    }
+        
+    #start {    
+        border-radius: 100px 100px 30px 30px;
+    }
+
+    #exit {
+        border-radius: 30px 30px 100px 100px;
+    }
+</style>
---
+++ SvelteKitVersion/src/routes/game/[id]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {page} from "$app/stores";
+    const {id} = $page.params;
+</script>
+
+<h1>game {id}</h1>
